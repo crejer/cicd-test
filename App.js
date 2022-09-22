bd3788fba7e4ab2f2e6cc45ca312aa33e87c653f
Jeroen Crevits <jeroen.crevits@sweetmustard.be>
2022-09-22 13:16:56 +0200

    edit gitlab

--- App.js
+++ App.js
@@ -67,7 +67,7 @@ const App: () => Node = () => {
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this screen and then come back to see your
-            edits. Hello
+            edits.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
